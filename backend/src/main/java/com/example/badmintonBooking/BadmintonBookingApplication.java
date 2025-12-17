d65601fd80821917366eb9407ab5d9841b188476
vishnuofficial004-ui <vishnuofficial004@gmail.com>
2025-12-18 00:19:32 +0530

    connection succesful with driver

---
+++ backend/src/main/java/com/example/badmintonBooking/BadmintonBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.badmintonBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadmintonBookingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/badmintonBooking/BadmintonBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.badmintonBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadmintonBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
